2b82b303713a01433f78c56368a137c4daa57a90
yy8305 <yy8305@kakao.com>
2023-11-14 15:49:07 +0900

    test(Discounts): 증정 메뉴를 저장하는 기능 테스트 코드 추가

--- src/test/java/christmas/domain/DiscountsTest.java
+++ src/test/java/christmas/domain/DiscountsTest.java
@@ -1,8 +1,10 @@
 package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 import christmas.constants.DiscountEvent;
+import christmas.constants.MenuBook;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,5 +33,26 @@ class DiscountsTest {
             new Discounts(null);
         });
     }
+
+    @DisplayName("추가 했던 증정 메뉴와 저장 되어 있는 증정 메뉴가 일치 한다.")
+    @Test
+    void testAddGiftMenu() {
+        MenuBook giftMenu = MenuBook.CHAMPAGNE;
+        Discounts discounts = new Discounts(List.of(DiscountEvent.GIFT));
+
+        discounts.addGiftMenu(giftMenu);
+
+        assertThat(discounts.getGiftMenus().getMenus())
+                .containsKey(new Menu(giftMenu));
+    }
+
+    @DisplayName("증정 메뉴로 null을 전달할 경우 예외가 발생 한다.")
+    @Test
+    void testAddGiftMenuNullExceptionCheck() {
+        Discounts discounts = new Discounts(List.of(DiscountEvent.GIFT));
+
+        assertThatNullPointerException().isThrownBy(() -> {
+            discounts.addGiftMenu(null);
+        });
     }
 }
