a1234c44da0311e847051bea2f08b70e6b5403e9
yy8305 <yy8305@kakao.com>
2023-11-15 12:45:21 +0900

    refactor(OrderCalculator): 테스트 코드를 할인이 있을경우, 없을 경우로 나누기

--- src/test/java/christmas/domain/OrderCalculatorTest.java
+++ src/test/java/christmas/domain/OrderCalculatorTest.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class OrderCalculatorTest extends TestDefault {
@@ -31,55 +32,59 @@ class OrderCalculatorTest extends TestDefault {
         orderMenus.addMenu(new Menu(MenuBook.ZERO_COLA), minOrderCount);
     }
 
-    @BeforeEach
-    void testInit() {
-        setCalender();
-    }
+    @Nested
+    @DisplayName("할인이 있을 경우")
+    class InCaseOfDiscount {
+        @BeforeEach
+        void testInit() {
+            setCalender();
+            setOrderMenus();
+            final LocalDate reservationDate = LocalDate.of(eventYear, eventMonth, 3);
+            calender.setReservationDate(reservationDate);
+            discounts = new Discounts(calender.getDiscountEventForDate(reservationDate));
+            calculator = new OrderCalculator(calender, orderMenus, discounts);
+            calculator.setDiscountsAmountByEvent();
+        }
 
-    @DisplayName("이벤트별 할인 금액 계산시 예상 했던 금액과 일치 한다.")
-    @Test
-    void testSetDiscountsAmountByEvent() {
-        setOrderMenus();
-        final LocalDate reservationDate = LocalDate.of(eventYear, eventMonth, 3);
-        final List<Integer> expectedDiscountAmounts = List.of(1_200, 4_046, 1_000, 25_000);
-        calender.setReservationDate(reservationDate);
-        discounts = new Discounts(calender.getDiscountEventForDate(reservationDate));
-        calculator = new OrderCalculator(calender, orderMenus, discounts);
+        @DisplayName("이벤트별 할인 금액 계산시 예상 했던 금액과 일치 한다.")
+        @Test
+        void testSetDiscountsAmountByEvent() {
+            final List<Integer> expectedDiscountAmounts = List.of(1_200, 4_046, 1_000, 25_000);
 
-        calculator.setDiscountsAmountByEvent();
+            for (Integer amount : expectedDiscountAmounts) {
+                assertThat(discounts.getAmountsByEvent()).containsValue(amount);
+            }
+        }
 
-        for (Integer amount : expectedDiscountAmounts) {
-            assertThat(discounts.getAmountsByEvent()).containsValue(amount);
+        @DisplayName("총 할인 금액은 31,246원 이다. (크리스마스 디데이 할인: -1,200원, 평일 할인: -4,046원, 특별 할인: -1,000원, 증정 이벤트: -25,000원)")
+        @Test
+        void testGetTotalDiscountsAmount() {
+            final Integer expectedTotalDiscountAmount = 31_246;
+
+            Integer actual = calculator.getTotalDiscountsAmount();
+
+            assertThat(actual).isEqualTo(expectedTotalDiscountAmount);
         }
     }
 
-    @DisplayName("적용된 할인 이벤트가 없을 경우 discounts에 저장된 이벤트는 없다.")
-    @Test
-    void testSetDiscountsAmountWithoutEvent() {
-        orderMenus = new MenuCart();
-        final LocalDate reservationDate = LocalDate.of(eventYear, eventMonth, 26);
-        calender.setReservationDate(reservationDate);
-        discounts = new Discounts(calender.getDiscountEventForDate(reservationDate));
-        calculator = new OrderCalculator(calender, orderMenus, discounts);
+    @Nested
+    @DisplayName("할인이 없을 경우")
+    class InCaseOfNoDiscount {
+        @BeforeEach
+        void testInit() {
+            setCalender();
+            orderMenus = new MenuCart();
+            final LocalDate reservationDate = LocalDate.of(eventYear, eventMonth, 26);
+            calender.setReservationDate(reservationDate);
+            discounts = new Discounts(calender.getDiscountEventForDate(reservationDate));
+            calculator = new OrderCalculator(calender, orderMenus, discounts);
+            calculator.setDiscountsAmountByEvent();
+        }
 
-        calculator.setDiscountsAmountByEvent();
-
-        assertThat(discounts.getAmountsByEvent().size()).isEqualTo(Settings.ZERO.getValue());
-    }
-
-    @DisplayName("총 할인 금액은 31,246원 이다. (크리스마스 디데이 할인: -1,200원, 평일 할인: -4,046원, 특별 할인: -1,000원, 증정 이벤트: -25,000원)")
-    @Test
-    void testGetTotalDiscountsAmount() {
-        final Integer expectedTotalDiscountAmount = 31_246;
-        setOrderMenus();
-        final LocalDate reservationDate = LocalDate.of(eventYear, eventMonth, 3);
-        calender.setReservationDate(reservationDate);
-        discounts = new Discounts(calender.getDiscountEventForDate(reservationDate));
-        calculator = new OrderCalculator(calender, orderMenus, discounts);
-        calculator.setDiscountsAmountByEvent();
-
-        Integer actual = calculator.getTotalDiscountsAmount();
-
-        assertThat(actual).isEqualTo(expectedTotalDiscountAmount);
+        @DisplayName("적용된 할인 이벤트가 없을 경우 discounts에 저장된 이벤트는 없다.")
+        @Test
+        void testSetDiscountsAmountWithoutEvent() {
+            assertThat(discounts.getAmountsByEvent().size()).isEqualTo(Settings.ZERO.getValue());
+        }
     }
 }
